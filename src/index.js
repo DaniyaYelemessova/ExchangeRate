43769bcb4ae01e344dba623c8bd44bb677ff4a74
Daniya Yelemessova <daniya@collingsmail.com>
2024-01-10 22:54:08 -0500

    Add handleform

--- src/index.js
+++ src/index.js
@@ -4,5 +4,21 @@ import './css/styles.css';
 import CurrencyService from './currency-service';
 
 
+function handleFormSubmission(e){
+  e.preventDefault();
+  const baseCurrency = document.querySelector("#baseCurrency").value;
+  const targetCurrency = document.querySelector("#convertedCurrency").value;
+  const amount = parseFloat(document.querySelector("#amount").value);
+  document.querySelector("#amount").value = null;
+
+  
+  if(!isNaN(amount)){
+    getCurrency(baseCurrency, targetCurrency, amount);
+  }else{
+    console.error("Invalid amount. Please enter a valid number");
+  }
+}
+
+
 document.getElementById("form").addEventListener("submit", handleFormSubmission);
 

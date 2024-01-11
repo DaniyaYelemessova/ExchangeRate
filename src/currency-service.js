023e473b428d2db5f37e0208fe43ec20a0ddef5a
Daniya Yelemessova <daniya@collingsmail.com>
2024-01-10 22:49:53 -0500

    Add api logic

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,3 +1,16 @@
 export default class CurrencyService{
-  
+  static async getCurrency(baseCurrency, targetCurrency){
+    try{
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCurrency}/${targetCurrency}`);
+      const jsonResponse = await response.json();
+      if(!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText}
+        ${jsonResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      return jsonResponse;
+    }catch(error){
+      return error;
+    }
+  }
 }
